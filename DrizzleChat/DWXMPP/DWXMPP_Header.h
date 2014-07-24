ddde9cd78c0d90bbf7dbe65884e1e4610d263d4b
DrizzleWang <wangyu9058@gmail.com>
2014-07-25 00:44:17 +0800

    Add ChatRoom

---
+++ DrizzleChat/DWXMPP/DWXMPP_Header.h
@@ -0,0 +1,20 @@
+//
+//  DWXMPP_Header.h
+//  DrizzleChat
+//
+//  Created by mitbbs on 14-3-14.
+//  Copyright (c) 2014年 DrizzleWang. All rights reserved.
+//
+
+//#ifndef DrizzleChat_DWXMPP_Header_h
+//#define DrizzleChat_DWXMPP_Header_h
+
+#import "DWXMPP_Core.h"
+#import "DWXMPP_Logging.h"
+////XMPPRoster处理类
+#import "DWXMPP_Core+DWXMPP_Roster.h"
+////DWXMPP调试处理类
+//#import "DWXMPP_Core+DWXMPP_Logging.h"
+
+#import "DWXMPP_Core+DWXMPP_Message.h"
+
